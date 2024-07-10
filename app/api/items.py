f3f5df2573db20f2dca8484cdddf1bd6c5a0436d
Hung Truong <hungtruongquoc@gmail.com>
2024-07-09 19:47:59 -0500

    Fixed the wrong route declaration

--- app/api/items.py
+++ app/api/items.py
@@ -5,6 +5,6 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/items/{item_id}")
+@router.get("/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
--- app/api/routes.py
+++ app/api/routes.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 from app.api import index, items
 
 router = APIRouter()
-router.include_router(index.router, prefix="/", tags=["index"])
+router.include_router(index.router, prefix="", tags=["index"])
 router.include_router(items.router, prefix="/items", tags=["items"])
