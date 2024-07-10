39b316bdc17703188e2ddccd47b1514d7ede32dd
Hung Truong <hungtruongquoc@gmail.com>
2024-07-09 19:55:25 -0500

    Fixed the missing DATABASE_URL

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "My FastAPI Application"
     API_V1_STR: str = "/api/v1"
-    DATABASE_URL: str
+    DATABASE_URL: str = ""
 
     class Config:
         env_file = ".env"
