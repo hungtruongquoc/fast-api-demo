2ca5350f20d2c904ef6cf7153833ea692ce4bc0b
Hung Truong <hungtruongquoc@gmail.com>
2024-07-20 22:16:38 -0500

    Added a new route for appointments

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from app.api import index, items
+from app.api import index, items, appointments
 from app.api.app_api_router import AppAPIRouter  # Assuming the CustomAPIRouter is defined here
 
 # Replace APIRouter with CustomAPIRouter
 router = AppAPIRouter()
 router.include_router(index.router, prefix="", tags=["index"])
 router.include_router(items.router, prefix="/items", tags=["items"])
-router.include_router(items.router, prefix="/appointments", tags=["appointments"])
+router.include_router(appointments.router, prefix="/appointments", tags=["appointments"])
