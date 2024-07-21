4e8bf892ced2b8892e4fc1730229a9e879e22c5a
Hung Truong <hungtruongquoc@gmail.com>
2024-07-21 10:09:57 -0500

    Fixed the wrong router

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from app.api import index, items, appointments, packages
+from app.api.appointments.appointments import router as appointments_router
 from app.api.app_api_router import AppAPIRouter  # Assuming the CustomAPIRouter is defined here
 
 # Replace APIRouter with CustomAPIRouter
 router = AppAPIRouter()
 router.include_router(index.router, prefix="", tags=["index"])
 router.include_router(items.router, prefix="/items", tags=["items"])
-router.include_router(appointments.router, prefix="/appointments", tags=["appointments"])
+router.include_router(appointments_router, prefix="/appointments", tags=["appointments"])
 router.include_router(packages.router, prefix="/packages", tags=["packages"])
