7bba5177930275a49130a7454e5c1b77e980d769
Hung Truong <hungtruongquoc@gmail.com>
2024-07-21 17:40:34 -0500

    Fixed the validator

--- app/api/decorators/api_key_validator.py
+++ app/api/decorators/api_key_validator.py
@@ -10,8 +10,7 @@ valid_api_keys = [settings.GROOMING_API_KEY, settings.ADMIN_API_KEY]
 
 def api_key_required(func: Callable):
     @wraps(func)
-    async def wrapper(*args, **kwargs):
-        request: Request = kwargs.get('request')
+    async def wrapper(*args, request: Request, **kwargs):
         api_key = request.query_params.get("customer")
         if valid_api_keys.index(api_key) == -1:
             raise HTTPException(status_code=403, detail="Access forbidden: Invalid API key")
