b82b6c4cad8da660a9a6d347c9485ed9eacf7c11
Hung Truong <hungtruongquoc@gmail.com>
2024-07-21 00:16:37 -0500

    Added packages router

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api import index, items, appointments
+from app.api import index, items, appointments, packages
 from app.api.app_api_router import AppAPIRouter  # Assuming the CustomAPIRouter is defined here
 
 # Replace APIRouter with CustomAPIRouter
@@ -7,3 +7,4 @@ router = AppAPIRouter()
 router.include_router(index.router, prefix="", tags=["index"])
 router.include_router(items.router, prefix="/items", tags=["items"])
 router.include_router(appointments.router, prefix="/appointments", tags=["appointments"])
+router.include_router(packages.router, prefix="/packages", tags=["packages"])
