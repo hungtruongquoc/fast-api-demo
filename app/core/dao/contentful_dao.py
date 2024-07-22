4c2315b5d675c242db9a9762805df220d726a898
Hung Truong <hungtruongquoc@gmail.com>
2024-07-22 09:00:39 -0500

    Try to fix this again

--- app/core/dao/contentful_dao.py
+++ app/core/dao/contentful_dao.py
@@ -46,10 +46,16 @@ class ContentfulDAO:
             logger.error(f"An error occurred: {e}")
             raise
 
+    def _merge_sys_and_fields(self, entry):
+        # Access the sys and fields attributes
+        sys_dict = entry.sys if isinstance(entry.sys, dict) else entry.sys.__dict__
+        fields_dict = entry.fields if isinstance(entry.fields, dict) else entry.fields.__dict__
+        return {**sys_dict, **fields_dict}
+
     def get_appointments(self):
         entries = self.cda_client.entries({'content_type': 'appointments'})
-        return list(map(lambda entry: {**entry.sys, **entry.fields}, entries))
+        return list(map(lambda entry: self._merge_sys_and_fields(entry), entries))
 
     def get_packages(self):
         entries = self.cda_client.entries({'content_type': 'package'})
-        return list(map(lambda entry: {**entry.sys, **entry.fields}, entries))
+        return list(map(lambda entry:self._merge_sys_and_fields(entry), entries))
