79629df4e316dd503ba90fdc5837965672991485
Hung Truong <hungtruongquoc@gmail.com>
2024-07-22 08:55:20 -0500

    Added a log for packages endpoint

--- app/api/packages/packages.py
+++ app/api/packages/packages.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
-
+import logging
 from app.api.dependencies.api_key_validator import api_key_validator
-from app.api.packages.stats.stats import stats_router
 from app.core.dependencies.contentful_service_injector import get_contentful_service
 from app.core.services.contentful_service import ContentfulService
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 
 @router.get("/", dependencies=[Depends(api_key_validator)])
@@ -14,4 +14,5 @@ async def get_packages(service: ContentfulService = Depends(get_contentful_servi
         entries = service.get_packages()
         return {"packages": entries}
     except Exception as e:
+        logger.error(f"An error in get packages endpoint: ", e)
         raise HTTPException(status_code=500, detail=str(e))
